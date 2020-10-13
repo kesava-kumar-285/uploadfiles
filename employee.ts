c20fc796355ba99311447c618b8e723180597019
kesava-kumar-285 <72065414+kesava-kumar-285@users.noreply.github.com>
2020-10-13 16:29:28 +0530

    Add files via upload

---
+++ employee.ts
@@ -0,0 +1,13 @@
+export class Employee
+{
+   id:number;
+   name:string;
+   dept:string;
+   age:number;
+   constructor(id:number,name:string,dept:string,age:number){
+       this.id=id;
+       this.name=name;
+       this.dept=dept;
+       this.age=age;
+   }
+}
